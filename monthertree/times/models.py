3e24a023339a0ab193b2afb08df393fa3957e022
xiaoping jin <jinxp@lenovo.com>
2014-04-23 17:25:10 +0800

    db

--- monthertree/times/models.py
+++ monthertree/times/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Time(models.Model):
+	handle = models.charField(max_length=200)
+	title = models.charField(max_length=200)
+	content = models.charField(max_length=200)
+	create_date = models.DataTimeField('content create date')
+	create_time = models.DateTimeField('content create time')
+	content_type = models.IntegerField(default=0)
+	photo = models.ImageField(upload_to='Photo')
+	audio = models.FilePathField(upload_to='Audio')
+
+	def __unicode__(self):
+		return self.content
