b8e509ce1b842b893ba17c85ce1fe96179887287
jinxp <youtaya@qq.com>
2014-08-14 00:50:26 +0800

    update

--- monthertree/monthertree/settings.py
+++ monthertree/monthertree/settings.py
@@ -24,17 +24,17 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR, "site_media", "media")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = "/site_media/media/"
+MEDIA_URL = "/media/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PACKAGE_DIR, "site_media", "static")
+STATIC_ROOT = os.path.join(PROJECT_DIR, "site_media", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/site_media/static/"
+STATIC_URL = "/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = (
--- monthertree/monthertree/wsgi.py
+++ monthertree/monthertree/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/home/jinxp/Documents/shell/mothertree/monthertree/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monthertree.settings")
 
 from django.core.wsgi import get_wsgi_application
