1bd809a13073a5013169c7e9807dfbd91a00f50f
youtaya <jxiaoping@gmail.com>
2014-12-01 00:04:05 +0800

    update

--- monthertree/dialogs/models.py
+++ monthertree/dialogs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Dialog(models.Model):
+    handle = models.CharField(max_length=100)
+    room_name = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    direct = models.IntegerField(default=0)
+    content = models.CharField(max_length=300)
+    create_date = models.CharField(max_length=200)
+    create_time = models.CharField(max_length=200)
+    content_type = models.IntegerField(default=0)
+    photo = models.ImageField(upload_to='Photo')
+    audio = models.FileField(upload_to='Audio')
+    deleted = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+    def __unicode__(self):
+        return self.content
+
+    @classmethod
+    def get_record_create_time(cls, time):
+        if create_time not in (None, ''):
+            query = cls.objects.get(create_time=time)
+            return query
+        return None
