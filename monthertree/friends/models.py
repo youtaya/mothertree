eecb7a746db3f2a53efba775f36b3d1ce4c036df
youtaya <jxiaoping@gmail.com>
2014-12-11 00:04:27 +0800

    update

--- monthertree/friends/models.py
+++ monthertree/friends/models.py
@@ -5,6 +5,7 @@ class Friend(models.Model):
 	username = models.CharField(max_length=200)
 	phone_mobile = models.CharField(max_length=200)
 	avatar = models.ImageField(upload_to='avatar')
+	description = models.CharField(max_length=200)
 	deleted = models.BooleanField(default=False)
 	updated = models.DateTimeField(auto_now_add=True)
 
