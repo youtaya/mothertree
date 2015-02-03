fc37860f0c4b859b697f25d124879770f3caf2d7
xiaoping jin <jinxp@lenovo.com>
2015-02-03 10:08:19 +0800

    opt

--- monthertree/users/admin.py
+++ monthertree/users/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from users.models import UserInfo
 
-admin.site.register(UserInfo)
+
+class UserInfoAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('user', {'fields':['user']}),
+		('nickname', {'fields': ['nickname']}),
+	]
+    
+admin.site.register(UserInfo, UserInfoAdmin)
