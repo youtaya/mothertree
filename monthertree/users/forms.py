da9ccefc98746610cb3b7a47403413c33c73a8df
xiaoping jin <jinxp@lenovo.com>
2015-03-31 16:12:49 +0800

    update

--- monthertree/users/forms.py
+++ monthertree/users/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class UploadFileForm(forms.Form):
 	image = forms.FileField()
+
+class UserInfoForm(forms.Form):
+    nickname = forms.CharField(max_length=24)
+    avatar = forms.FileField()
